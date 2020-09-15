491f9b186549824e58963a0ca82b8a121e140099
gregara6 <DmYK@rambler.ru>
2020-09-15 15:42:13 +0300

    MyTetraCommit

---
+++ base/1600173333mg1jo08z52/16001735025934i7u8ny.bat
@@ -0,0 +1,5 @@
+RC5SIMPºÓíúE=Þ
+’uTëÓß)bÒí]é^8ÏQÊ-oäÙ.8Hé«Õ’è4üD€Ébà-š
+{r_ÔÚg* ÃîXˆ~óuÎ;ÞiÍºqÉÈ¨½À2¡ÇêZô
+âkWd™ÛòïŠœòs0	~S¤2¦‡Öú	ÿæäÄOö¯B•
+ë=pLþ4Y±é5–fOÿÉê–FÂC2Ah™gPˆ’}Éõ[¾A·b‹ÒÈOyK;ú¡‘…Y×ÊUç&28Ð
